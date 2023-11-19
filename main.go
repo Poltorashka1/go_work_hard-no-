addbdfe94ecb1e0d7a884fa7cd480ef07de617ed
Killy <sanfor228@mail.ru>
2023-11-19 13:10:13 +0400

    first task solution

--- main.go
+++ main.go
@@ -1,4 +1,23 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+)
+
 func main() {
+	x := "12"
+	strConv, err := strconv.Atoi(x)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Printf("Before conv %v - %T\n", x, x)
+	fmt.Printf("After conv %v - %T\n", strConv, strConv)
+	fmt.Println()
+	y := 24
+	intConv := strconv.Itoa(y)
+	fmt.Printf("Before conv %v - %T\n", y, y)
+	fmt.Printf("After conv %v - %T\n", intConv, intConv)
+
 }
